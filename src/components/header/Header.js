d2069b6192e7d3777fdf93363933e060a39713f6
freefireman <beluh.c.s@gmail.com>
2020-12-30 20:50:10 +0200

    some fix))

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 import {useSelector} from "react-redux";
 
 export const Header = (props) => {
-    const { cart, wishlist} = useSelector(({cart: {cart}, wishlist: {wishlist}}) => ({cart, wishlist}))
+    const { cart, wishlist, totalPrices} = useSelector(({cart: {cart}, wishlist: {wishlist, totalPrices}}) =>
+        ({cart, wishlist,totalPrices}))
+
     return (
         <header className='d-flex justify-between align-center'>
             <h2>Wellcome to ...shop</h2>
             <div className='d-flex justify-between'>
-                <div className='mx-10' title='cart.reduce()'>wishlist: {wishlist.length}</div>
+                <div className='mx-10' title={totalPrices}>wishlist: {wishlist.length}</div>
                 <div className='mx-10' title='wishlist.reduce()'>cart: {cart.length}</div>
             </div>
 
